c3e8737ffffcd8d115ef8e6848b64bd00c2e15aa
Raquel Alamán <raquel.alaman@itb.cat>
2025-10-17 13:00:10 +0200

    Added solution of Exercise 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Ac06Implementacio {
+    // Exercise 1
+    // PRECONDITION: The user is prompted to input two integers.
+    public class Program {
+        public static void Main()
+        {
+            // Constants
+            const string MsgInputOne = "Enter the first integer:";
+            const string MsgInputTwo = "Enter the second integer:";
+            const string MsgError = "Error in format";
+            const string MsgResult = "The sum is:";
+            
+            // Variables
+            int numOne, numTwo, sum;
+            bool isInt;
+            Console.WriteLine(MsgInputOne);
+            isInt = Int32.TryParse(Console.ReadLine(), out numOne);
+
+            // Input and data validation
+            if (isInt)
+            {
+                Console.WriteLine(MsgInputTwo);
+                isInt = Int32.TryParse(Console.ReadLine(), out numTwo);
+                if (isInt)
+                {
+                    sum = numOne + numTwo;
+                    Console.WriteLine(MsgResult + " " + sum);
+                }
+                else
+                {
+                    Console.WriteLine(MsgError);
+                }
+            }
+            else
+            {
+                Console.WriteLine(MsgError);
+            }
+        }
+    }
+    // POSTCONDITION: 'sum' contains the sum of 'num1' and 'num2', and it is // displayed.
+}
